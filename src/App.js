c0015c9fa6029f354e22c528959ce5037961ee41
Sanjana Sridhar <sanjana.sridhar@outlook.com>
2019-10-13 00:56:01 -0700

    new changes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,11 @@ function App() {
       <Header />
       <div className={"bigBrain_main"}>
         <div className={"headertext"}>
-          Oregano
-        </div>
-        <div>
-        <button className={"button_ovolacto"} type="button" onClick = "alert('Ovo-Lacto-Vegetarian')">Ovo-Lacto-Vegetarian</button>
-        <button className={"button_vegetarian"} type="button" onClick = "alert('Vegetarian')">Vegetarian</button>
-        <button className={"button_vegan"} type="button" onClick="alert('Vegan')">Vegan</button>
-        <button className={"button_glutenfree"} type="button" onClick="alert('Gluten Free')">Gluten-Free</button>
+          Oregano   
+        <button className={"button"} type="button" onClick = "alert('Ovo-Lacto-Vegetarian')">Ovo-Lacto-Vegetarian</button>&nbsp;
+        <button className={"button"} type="button" onClick = "alert('Vegetarian')">Vegetarian</button>&nbsp;
+        <button className={"button"} type="button" onClick="alert('Vegan')">Vegan</button>&nbsp;
+        <button className={"button"} type="button" onClick="alert('Gluten Free')">Gluten-Free</button>
         </div>
       </div>
     </div>
