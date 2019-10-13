8060f4203d6911a087cf84e998bae9e8cf039284
ramyac6 <ramchalla6@gmail.com>
2019-10-13 11:35:06 -0700

    Updates

---
+++ precache-manifest.04ae8a76dbefe74da69a41f0266c5379.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2756dff36043dbf86894f816e0b5a5fd",
+    "url": "/oregano-online/index.html"
+  },
+  {
+    "revision": "e832a56d742ff57f6571",
+    "url": "/oregano-online/static/css/main.461887fc.chunk.css"
+  },
+  {
+    "revision": "66d346fa5d9dca2473a0",
+    "url": "/oregano-online/static/js/2.fd742fd4.chunk.js"
+  },
+  {
+    "revision": "e832a56d742ff57f6571",
+    "url": "/oregano-online/static/js/main.285fd8f4.chunk.js"
+  },
+  {
+    "revision": "87db9e3aeee7749cf90f",
+    "url": "/oregano-online/static/js/runtime-main.946460f3.js"
+  },
+  {
+    "revision": "d9cce9790e813925732c50aa34507ded",
+    "url": "/oregano-online/static/media/spices_background.d9cce979.jpg"
+  }
+]);
