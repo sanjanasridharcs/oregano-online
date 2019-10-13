7c7576e2fd37223ff53469c323e31777c69f3f5d
Sanjana Sridhar <sanjana.sridhar@outlook.com>
2019-10-13 09:33:43 -0700

    subpage changes

--- src/BasicInfo.js
+++ src/BasicInfo.js
@@ -3,9 +3,24 @@ import './BasicInfo.css';
 import React, { Component } from 'react';
 
 class BasicInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+ 
   render() {
     return (
-        <div className="BasicInfo"> </div>
+      <div>
+        <div className={"subtextleft"}>
+          {this.props.name}<br></br>
+          {this.props.add}
+        </div>
+        <div className={"subtextright"}>
+          Rating: Star Icons yay<br></br>
+          Read Reviews
+        </div>
+      </div>
       );
   }
 }
--- src/RealComp.js
+++ src/RealComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
+import BasicInfo from './BasicInfo';
 
 class RealApp extends Component {
     constructor(props) {
@@ -11,7 +12,7 @@ class RealApp extends Component {
     }
     
     display(name) {
-        alert(name);
+        this.setPage(1);
     }
 
     setPage(page) {
@@ -24,19 +25,36 @@ class RealApp extends Component {
     renderPage() {
         if (this.state.page === 0) { // the home page
             return (
-            <div className={"bigBrain_main"}>
-                <div className={"headertext"}>
-                    <div>
-                    Oregano   
-                    </div>
-                    <div>
-                    <button className={"button"} type="button" onClick={() => {this.display('Ovo-Lacto-Vegetarian')}}>Ovo-Lacto-Vegetarian</button>&nbsp;
-                    <button className={"button"} type="button" onClick={() => {this.display('Vegetarian')}}>Vegetarian</button>&nbsp;
-                    <button className={"button"} type="button" onClick={() => {this.display('Vegan')}}>Vegan</button>&nbsp;
-                    <button className={"button"} type="button" onClick={() => {this.display('Gluten Free')}}>Gluten-Free</button>
+                <div className={"bigBrain_main"}>
+                    <div className={"headertext"}>
+                        <div>
+                            Oregano   
+                        </div>
+                        <div>
+                            <button className={"button"} type="button" onClick={() => {this.display('Ovo-Lacto-Vegetarian')}}>Ovo-Lacto-Vegetarian</button>&nbsp;
+                            <button className={"button"} type="button" onClick={() => {this.display('Vegetarian')}}>Vegetarian</button>&nbsp;
+                            <button className={"button"} type="button" onClick={() => {this.display('Vegan')}}>Vegan</button>&nbsp;
+                            <button className={"button"} type="button" onClick={() => {this.display('Gluten Free')}}>Gluten-Free</button>
+                        </div>
                     </div>
                 </div>
-            </div>);
+            );
+        } else if (this.state.page === 1) {
+            return (
+                <div className={"bigBrain_sub"}>
+                    <div className={"subpage"}>
+                        <div  className={"subheader_text"}>
+                            Ovo-Lacto-Vegetarian
+                        </div>
+                        <BasicInfo 
+                            name={"ABC"} 
+                            add={"Address: 1234 NE UW St Seattle, WA"}
+                        />
+                        <BasicInfo />
+                        <BasicInfo />
+                    </div>
+                </div>
+            )
         }
         return (""); // an empty page
     }
