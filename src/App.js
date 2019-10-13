99afda6a14ef40e4b14ef3c064171dca84fe1c73
Kuro <body.identity@outlook.com>
2019-10-13 08:32:18 -0700

    Dpushing

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import './App.css';
 import Header from './Header';
 
 function App() {
+  function display(name) {
+    alert(name);
+  }
   return (
     <div className="App">
       <Header />
       <div className={"bigBrain_main"}>
         <div className={"headertext"}>
-          Oregano   
           <div>
-            <button className={"button"} type="button" onClick = {() => {alert('Ovo-Lacto-Vegetarian')}}>Ovo-Lacto-Vegetarian</button>&nbsp;
-            <button className={"button"} type="button" onClick = {() => {alert('Vegetarian')}}>Vegetarian</button>&nbsp;
-            <button className={"button"} type="button" onClick={() => {alert('Vegan')}}>Vegan</button>&nbsp;
-            <button className={"button"} type="button" onClick={() => {alert('Gluten Free')}}>Gluten-Free</button>
+           Oregano   
+          </div>
+          <div>
+            <button className={"button"} type="button" onClick={() => {display('Ovo-Lacto-Vegetarian')}}>Ovo-Lacto-Vegetarian</button>&nbsp;
+            <button className={"button"} type="button" onClick={() => {display('Vegetarian')}}>Vegetarian</button>&nbsp;
+            <button className={"button"} type="button" onClick={() => {display('Vegan')}}>Vegan</button>&nbsp;
+            <button className={"button"} type="button" onClick={() => {display('Gluten Free')}}>Gluten-Free</button>
           </div>
         </div>
       </div>
