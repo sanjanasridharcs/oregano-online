0524600d25fc96983858358c79191fc22543d093
Kuro <body.identity@outlook.com>
2019-10-12 21:40:13 -0700

    pushing header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import bg1 from "./spices_background.jpg";
 import './App.css';
 import './font/amaticsc-bold-webfont.woff';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div className={"bigBrain_main"}>
         <div className={"headertext"}>
           Oregano
